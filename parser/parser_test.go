14059c59b91a54c8be0dd02d434b4e58af0d6545
Ryan Brushett <ryan.brushett@shopify.com>
2016-12-09 22:40:53 -0500

    Add parsing for prefix operators like bang and minus

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -3,6 +3,8 @@ package parser
 import (
 	"testing"
 
+	"fmt"
+
 	"github.com/ryanbrushett/interpreter/ast"
 	"github.com/ryanbrushett/interpreter/lexer"
 )
@@ -163,4 +165,57 @@ func TestIntegerLiteralExpression(t *testing.T) {
 	if literal.TokenLiteral() != "5" {
 		t.Errorf("TokenLiteral is not %s, got %s", "5", literal.TokenLiteral())
 	}
+}
+
+func TestParsingPrefixExpressions(t *testing.T) {
+	prefixTest := []struct {
+		input        string
+		operator     string
+		integerValue int64
+	}{
+		{"!5;", "!", 5},
+		{"-15", "-", 15},
+	}
+
+	for _, test := range prefixTest {
+		l := lexer.New(test.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		if len(program.Statements) != 1 {
+			t.Fatalf("Program does not contain 1 statement. Got %d", len(program.Statements))
+		}
+		stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+		if !ok {
+			t.Fatalf("stmt is not an ExpressionStatement, got %T", program.Statements[0])
+		}
+		exp, ok := stmt.Expression.(*ast.PrefixExpression)
+		if !ok {
+			t.Fatalf("stmt is not a prefixexpression. got %T", stmt.Expression)
+		}
+		if exp.Operator != test.operator {
+			t.Fatalf("exp.Operator is not %s. got %s", test.operator, exp.Operator)
+		}
+		if !testIntegerLiteral(t, exp.Right, test.integerValue) {
+			return
+		}
+	}
+}
+
+func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
+	integ, ok := il.(*ast.IntegerLiteral)
+	if !ok {
+		t.Errorf("il not an integer literal, got %T", il)
+		return false
+	}
+	if integ.Value != value {
+		t.Errorf("integ.value is not %d. got %s", value, integ.Value)
+		return false
+	}
+	if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
+		t.Errorf("integ.TokenLiteral is not %d, got %s", value, integ.TokenLiteral())
+		return false
+	}
+	return true
 }
