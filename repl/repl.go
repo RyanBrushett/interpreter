aa27c79eef33d22e5a00028441adb555da04adf6
Ryan Brushett <ryan.brushett@shopify.com>
2016-12-25 01:10:09 -0330

    Extend repl

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ryanbrushett/interpreter/lexer"
 	"github.com/ryanbrushett/interpreter/parser"
+	"github.com/ryanbrushett/interpreter/evaluator"
 )
 
 const PROMPT = ">> "
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
