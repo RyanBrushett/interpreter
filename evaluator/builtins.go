ed2768d5dc8260732ad1108c569bc13ac3146696
Ryan Brushett <ryan.brushett@shopify.com>
2017-01-29 16:35:51 -0500

    Add evaluating hash literals

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,10 @@
 package evaluator
 
-import "github.com/ryanbrushett/interpreter/object"
+import (
+	"fmt"
+
+	"github.com/ryanbrushett/interpreter/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -81,6 +85,14 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -162,6 +162,9 @@ func TestErrorHandling(t *testing.T) {
 		}, {
 			`"Hello" - "World"`,
 			"unknown operator: STRING - STRING",
+		}, {
+			`{"name": "Monkey"}[fn(x) { x }];`,
+			"unusable as hash key: FUNCTION",
 		},
 	}
 	for _, test := range tests {
@@ -367,6 +370,82 @@ func TestArrayIndexExpressions(t *testing.T) {
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
+func TestHashLiterals(t *testing.T) {
+	input := `let two = "two";
+	{
+	    "one": 10 - 9,
+	    two: 1 + 1,
+	    "thr" + "ee": 6 / 2,
+	    4: 4,
+	    true: 5,
+	    false: 6
+	}`
+
+	evaluated := testEval(input)
+	result, ok := evaluated.(*object.Hash)
+	if !ok {
+		t.Fatalf("Eval didn't return a hash. got %T (%+v)", evaluated, evaluated)
+	}
+	expected := map[object.HashKey]int64{
+		(&object.String{Value: "one"}).HashKey():   1,
+		(&object.String{Value: "two"}).HashKey():   2,
+		(&object.String{Value: "three"}).HashKey(): 3,
+		(&object.Integer{Value: 4}).HashKey():      4,
+		TRUE.HashKey():                             5,
+		FALSE.HashKey():                            6,
+	}
+	if len(result.Pairs) != len(expected) {
+		t.Fatalf("Hash has wrong number of pairs. Got %d", len(result.Pairs))
+	}
+	for expectedKey, expectedValue := range expected {
+		pair, ok := result.Pairs[expectedKey]
+		if !ok {
+			t.Error("No pair for given key")
+		}
+		testIntegerObject(t, pair.Value, expectedValue)
+	}
+}
+
+func TestHashIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			`{"foo": 5}["foo"]`,
+			5,
+		}, {
+			`{"foo": 5}["bar"]`,
+			nil,
+		}, {
+			`let key = "foo"; {"foo": 5}[key]`,
+			5,
+		}, {
+			`{}["foo"]`,
+			nil,
+		}, {
+			`{5: 5}[5]`,
+			5,
+		}, {
+			`{true: 5}[true]`,
+			5,
+		}, {
+			`{false: 5}[false]`,
+			5,
+		},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		integer, ok := test.expected.(int)
 		if ok {
 			testIntegerObject(t, evaluated, int64(integer))
 		} else {
